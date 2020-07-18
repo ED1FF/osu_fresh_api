feec553ede2b0ec095928c8aa53e8aba6f57aaaa
ED1FF <alekarp11@gmail.com>
2020-07-19 00:36:26 +0300

    Add seeds and fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api do
-    resources :users
-    post 'user_token' => 'user_token#create'
-  end
+  resources :users
+  post 'user_token' => 'user_token#create'
 end
